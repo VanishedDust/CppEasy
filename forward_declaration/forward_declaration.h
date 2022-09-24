aa592cfc0e644e48e25911cac0955e8bdb9b7102
VanishedDust <liuqianggege1@gmail.com>
2022-09-25 00:02:43 +0800

    Update forward_declaration.h

--- forward_declaration/forward_declaration.h
+++ forward_declaration/forward_declaration.h
@@ -1,4 +1,10 @@
 #pragma once
+
+/// <summary>
+/// https://blog.csdn.net/Kang_TJU/article/details/75290731
+/// https://blog.csdn.net/luoshabugui/article/details/116134043
+/// </summary>
+
 struct B
 {
 
